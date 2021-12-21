e4cf0cbe77a2021124e1982ba7f0d1845f86a4d3
Lyz03 <lizoe@netcourrier.com>
2021-12-21 12:51:32 +0100

    exo 125

---
+++ app.js
@@ -0,0 +1,31 @@
+window.addEventListener("keydown", function (e) {
+    switch (e.keyCode) {
+        case 65:
+            $('audio[data-key="65"]')[0].play();
+            break
+        case 83:
+            $('audio[data-key="83"]')[0].play();
+            break
+        case 68:
+            $('audio[data-key="68"]')[0].play();
+            break
+        case 70:
+            $('audio[data-key="70"]')[0].play();
+            break
+        case 71:
+            $('audio[data-key="71"]')[0].play();
+            break
+        case 72:
+            $('audio[data-key="72"]')[0].play();
+            break
+        case 74:
+            $('audio[data-key="74"]')[0].play();
+            break
+        case 75:
+            $('audio[data-key="75"]')[0].play();
+            break
+        case 76:
+            $('audio[data-key="76"]')[0].play();
+            break
+    }
+})
